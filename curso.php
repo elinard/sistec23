33b974f4686500673f6ac508febdcbac0758edee
Elinardy Andrade <elinardy@gmail.com>
2023-11-16 19:26:26 -0300

    removendo font

--- curso.php
+++ curso.php
@@ -46,10 +46,14 @@ include_once("process/cad_curso.php");
                 <!-- <a class="link-icon" href="#">
                   <ion-icon name="create-outline" size="large"></ion-icon>
                 </a> -->
-                <form action="process/cad_curso.php" method="POST">
+                <form action="process/cad_curso.php" method="POST" id="deleteForm">
                   <input type="hidden" name="id_curso" value="<?= $curso["ID"] ?>">
                   <input type="hidden" name="type" value="delete">
-                  <button type="submit" class="btn btn-hover" id="liveAlertBtn"><ion-icon name="trash-outline" size="large"></ion-icon></button>
+                  <!-- <button type="submit" class="btn btn-hover" id="liveAlertBtn"><ion-icon name="trash-outline" size="large"></ion-icon></button> -->
+                  <button type="button" class="btn btn-hover" data-bs-toggle="modal" data-bs-target="#confirmDeleteModal" data-id="<?= $curso["ID"] ?>">
+                    <ion-icon name="trash-outline" size="large"></ion-icon>
+                  </button>
+
                 </form>
               </td>
             </tr>
@@ -59,6 +63,46 @@ include_once("process/cad_curso.php");
     </div>
   </div>
 </div>
+<!-- Modal de Confirmação -->
+<div class="modal fade" id="confirmDeleteModal" tabindex="-1" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="confirmDeleteModalLabel">Confirmação de Exclusão</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Tem certeza de que deseja excluir este curso?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+        <button type="button" class="btn btn-danger" id="confirmDeleteBtn">Excluir</button>
+      </div>
+    </div>
+  </div>
+</div>
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    var confirmDeleteBtn = document.getElementById('confirmDeleteBtn');
+    var deleteForm = document.getElementById('deleteForm');
+
+    confirmDeleteBtn.addEventListener('click', function() {
+      // Obter o ID do curso
+      var cursoId = confirmDeleteBtn.getAttribute('data-id');
+
+      // Converter o ID para inteiro
+      cursoId = parseInt(cursoId, 10);
+
+      // Adicionar o ID ao formulário de exclusão
+      deleteForm.querySelector('input[name="id_curso"]').value = cursoId;
+
+      // Enviar o formulário de exclusão
+      deleteForm.submit();
+    });
+  });
+</script>
+
+
 <?php
 include_once('templates/footer.php');
 ?>
--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@ function confirmDelete() {
   // Aqui você pode adicionar a lógica para enviar os dados para a página PHP
   document.getElementById("form-del-curso").submit();
 }
+
