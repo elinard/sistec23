f82376a899f1fcdff60a2c10c3d7dd9400fbba66
Elinardy Andrade <elinardy@gmail.com>
2023-12-11 18:59:16 -0300

    1

--- curso.php
+++ curso.php
@@ -13,18 +13,7 @@ include_once("process/cad_curso.php");
           <label for="nome_curso" class="form-label"><strong>Curso</strong></label>
           <input type="TEXT" class="form-control" id="nome_curso" name="nome_curso" placeholder="nome do curso">
         </div>
-        <!-- <div class="col-md-4">
-          <label for="sigla" class="form-label"><strong>Sigla</strong></label>
-          <input type="text" class="form-control upper" id="sigla">
-        </div>
-        
-        <div class="col-md-8">
-          <label for="selecao" class="form-label"><strong>Seleção</strong></label>
-          <select id="selecao" class="form-select">
-            <option selected>2024</option>
-            <option>2025</option>
-         </select> 
-        </div>-->
+
         <div class="col-md-12 d-grid gap-2">
           <button type="submit" class="btn btn-dark" id="liveAlertBtn">Cadastrar</button>
         </div>
@@ -41,22 +30,17 @@ include_once("process/cad_curso.php");
         </thead>
         <tbody class="table-group-divider">
           <?php foreach ($cursos as $curso) : ?>
-            <tr>
+            <tr id="linha-<?php echo $curso["ID"] ?>">
               <th scope="row" class="align-middle"><?= $curso["ID"] ?></th>
               <th scope="row" class="align-middle"><?= $curso["nome"] ?></th>
               <td>
                 <!-- <a class="link-icon" href="#">
                   <ion-icon name="create-outline" size="large"></ion-icon>
                 </a> -->
-                <button type="button" class="btn btn-hover btn-abrir-modal-del" data-bs-toggle="modal" data-bs-target="#confirmDeleteModal" data-id="<?php echo $curso["ID"] ?>">
+                <button type="button" class="btn btn-hover btn-abrir-modal-del" onclick="return confirma();" data-bs-toggle="modal" data-bs-target="#confirmDeleteModal" data-id="<?php echo $curso["ID"] ?>">
                   <ion-icon name="trash-outline" size="large"></ion-icon>
                 </button>
-                <form action="process/cad_curso.php" method="POST" id="deleteForm">
-                  <input type="hidden" name="id_curso" value="<?= $curso["ID"] ?>">
-                  <input type="hidden" name="type" value="delete">
-                  <!-- <button type="submit" class="btn btn-hover" id="liveAlertBtn"><ion-icon name="trash-outline" size="large"></ion-icon></button> -->
-
-                </form>
+                
               </td>
             </tr>
           <?php endforeach; ?>
@@ -66,40 +50,21 @@ include_once("process/cad_curso.php");
   </div>
 </div>
 <!-- Modal de Confirmação -->
-<div class="modal fade" id="confirmDeleteModal" tabindex="-1" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="confirmDeleteModalLabel">Confirmação de Exclusão</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <p>Tem certeza de que deseja excluir este curso?</p>
-        <form class="form-del-curso">
-          <input class="form-control" type="text" name="id_curso" readonly>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-        <button type="button" class="btn btn-danger" id="confirmDeleteBtn">Excluir</button>
-      </div>
-    </div>
-  </div>
-</div>
+<?php include_once('templates/modal.php'); ?>
 <script>
-  $(document).ready(function() {
-    // Quando o botão for clicado
-    $('.btn-abrir-modal-del').click(function() {
-      // Obtenha o valor do data-id do elemento pai (curso)
-      var cursoID = $(this).data('id');
+    function confirma(){
+      ;
 
-      // Insira o valor do data-id no modal onde desejar
-      $('#confirmDeleteModal').find('.form-del-curso input[name="id_curso"]').val(cursoID);
+      if(x==true){
+    			header("location: index.php");
+          return true;
 
-      // Abra o modal
-      $('#confirmDeleteModal').modal('show');
-    });
-  });
+      }
+      else
+      {
+          return false;
+      }
+  }
 </script>
 
 
---
+++ templates/modal.php
@@ -0,0 +1,25 @@
+<div class="modal fade" id="confirmDeleteModal" tabindex="-1" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirmDeleteModalLabel">Confirmação de Exclusão</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>Tem certeza de que deseja excluir este curso?</p>
+                <form id="form-del-curso" action="process/cad_curso.php" method="POST">
+                    <input type="text" name="id_curso" value="<?= $curso["ID"] ?>">
+                    <input type="text" name="type" value="delete">
+                    <button type="submit" class="btn btn-hover" id="liveAlertBtn">
+
+                    </button>
+
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-danger" id="confirmDeleteBtn">Excluir</button>
+            </div>
+        </div>
+    </div>
+</div>
